c33d06d04a496fa890e66423856d0c6ea89d015a
Shruthigowdakj <120366375+Shruthigowdakj@users.noreply.github.com>
2023-02-09 12:28:15 +0530

    Update cse.ps1

--- cse.ps1
+++ cse.ps1
@@ -1,14 +1,16 @@
 # Define the download URL for the Visual Studio Code installation file
-$VSCodeDownloadUrl = "https://update.code.visualstudio.com/latest/win32-x64-user/stable"
+//$VSCodeDownloadUrl = "https://update.code.visualstudio.com/latest/win32-x64-user/stable"
 
 # Define the target location for the Visual Studio Code installation file
-$VSCodeInstaller = "$env:TEMP\vscode-installer.exe"
+//$VSCodeInstaller = "$env:TEMP\vscode-installer.exe"
 
 # Download the Visual Studio Code installation file
-Invoke-WebRequest -Uri $VSCodeDownloadUrl -OutFile $VSCodeInstaller
+//Invoke-WebRequest -Uri $VSCodeDownloadUrl -OutFile $VSCodeInstaller
 
 # Install Visual Studio Code
-Start-Process -FilePath $VSCodeInstaller -Wait
+//Start-Process -FilePath $VSCodeInstaller -Wait//
+ Install-VSCode.ps1 -Architecture 32-bit
+    Installs Visual Studio Code (32-bit) and the powershell extension.
 
 # Clean up the installation file
 Remove-Item $VSCodeInstaller
